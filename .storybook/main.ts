7c5fdaca9f6a98c44c5d5adf605d47d1801a374b
Leonardo <leo.hentschke@hotmail.com>
2024-02-22 16:04:31 -0300

    Theme

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,6 +11,7 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
+  staticDirs: ["../app"],
   framework: {
     name: "@storybook/nextjs",
     options: {},
